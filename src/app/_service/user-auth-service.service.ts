5c875ee876a94f77a0f219a2ec2cce32b293b496
Dharmesh <161964320+dharmesh015@users.noreply.github.com>
2025-03-31 01:42:32 +0530

    add  new  method -- user-auth-service.service.ts

--- src/app/_service/user-auth-service.service.ts
+++ src/app/_service/user-auth-service.service.ts
@@ -1,49 +1,4 @@
-// import { Injectable } from '@angular/core';
-// import { BehaviorSubject } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class UserAuthServiceService {
-  
-//   constructor() { }
-
-
-  
-
-//   public setRoles(roles: any[]): void {
-//     localStorage.setItem('roles', JSON.stringify(roles));
-//   }
-
-//   public getRoles(): any[] {
-//     const roles = localStorage.getItem('roles');
-//     return roles ? JSON.parse(roles) : []; 
-//   }
-
-//   public setToken(jwtToken: string): void {
-//     localStorage.setItem('jwtToken', jwtToken); 
-//   }
-
-//   public getToken() {
-//     return localStorage.getItem('jwtToken') ; 
-//   }
-
-//   public clear(): void {
-//     localStorage.removeItem('jwtToken'); 
-//     localStorage.removeItem('roles'); 
-//   }
-
-//   public isLoggedIn(): boolean {
-//     return !!this.getRoles() && !!this.getToken(); 
-// }
-
-// public isAdmin(){
-//   const roles:any[]=this.getRoles();
-//   // console.log(roles);
-//   return roles[0].roleName==='Admin';
-// }
-
-// }
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
